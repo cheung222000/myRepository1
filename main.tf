97f41999ed3af887539535c88bbaf7f4b6515358
cheung222000 <cheung222000@gmail.com>
2026-03-06 11:25:47 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,29 +20,29 @@ variable "prefix" {
   default = "tfvmex"
 }
 
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "myAzureResourceGroup1" {
   name     = "${var.prefix}-resources"
   location = "West Europe"
 }
 
-resource "azurerm_virtual_network" "main" {
+resource "azurerm_virtual_network" "myAzureVirtualNetwork1" {
   name                = "${var.prefix}-network"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.myAzureResourceGroup1.location
+  resource_group_name = azurerm_resource_group.myAzureResourceGroup1.name
 }
 
 resource "azurerm_subnet" "internal" {
   name                 = "internal"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.main.name
+  resource_group_name  = azurerm_resource_group.myAzureResourceGroup1.name
+  virtual_network_name = azurerm_virtual_network.myAzureVirtualNetwork1.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
-resource "azurerm_network_interface" "main" {
+resource "azurerm_network_interface" "myAzureNetworkInterface1" {
   name                = "${var.prefix}-nic"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.myAzureResourceGroup1.location
+  resource_group_name = azurerm_resource_group.myAzureResourceGroup1.name
 
   ip_configuration {
     name                          = "testconfiguration1"
