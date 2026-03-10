c25b56b5c0c20a12b402ca87211969d1a41112ea
cheung222000 <cheung222000@gmail.com>
2026-03-10 16:56:28 +0000

    Update vpngateway.tf

--- vpngateway.tf
+++ vpngateway.tf
@@ -1,12 +1,12 @@
-resource "azurerm_resource_group" "example" {
-  name     = "localNetworkGWTest"
-  location = "West Europe"
+resource "azurerm_resource_group" "myAzureResourceGroup2" {
+  name     = "shared-expressroute-prod-uksouth-dataservices-nonprod"
+  location = var.location
 }
 
-resource "azurerm_local_network_gateway" "home" {
-  name                = "backHome"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  gateway_address     = "12.13.14.15"
+resource "azurerm_local_network_gateway" "myAzureLocalNetworkGateway" {
+  name                = "145.43.244.136-dataservices-nonprod-uksouth-local-network-gateway"
+  resource_group_name = azurerm_resource_group.myAzureResourceGroup2.name
+  location            = azurerm_resource_group.myAzureResourceGroup2.location
+  gateway_address     = "145.43.244.136"
   address_space       = ["10.0.0.0/16"]
 }
