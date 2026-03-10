bb9b7e818d9bd4290633d304fd3e8fe7691a5d98
cheung222000 <cheung222000@gmail.com>
2026-03-10 15:37:23 +0000

    Editing branch 3 (#41)
    * Update backend.tf
    
    * Update main.tf

--- backend.tf
+++ backend.tf
@@ -1,1 +1,14 @@
-
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~> 4.54.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "terraform-rg"
+    storage_account_name = "tfstate889243219119"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
