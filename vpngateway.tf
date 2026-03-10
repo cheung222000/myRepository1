035892afbee1aa0e66a48310aa8c7c66d45e1c55
cheung222000 <cheung222000@gmail.com>
2026-03-10 16:12:37 +0000

    Update vpngateway.tf

--- vpngateway.tf
+++ vpngateway.tf
@@ -8,5 +8,5 @@ resource "azurerm_local_network_gateway" "home" {
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
   gateway_address     = "12.13.14.15"
-  address_space       = ["10.0.0.0/16"]
+  address_space       = [""]
 }
