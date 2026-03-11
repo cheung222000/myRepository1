b6a29164031ee714ec28320acd2935fb833c563e
cheung222000 <cheung222000@gmail.com>
2026-03-11 09:35:39 +0000

    Update vpngateway.tf

--- vpngateway.tf
+++ vpngateway.tf
@@ -2,7 +2,7 @@ resource "azurerm_resource_group" "myAzureResourceGroup2" {
   name     = "shared-expressroute-prod-uksouth-dataservices-nonprod"
   location = var.location
 }
-/*
+
 resource "azurerm_local_network_gateway" "myAzureLocalNetworkGateway" {
   name                = "145.43.244.136-dataservices-nonprod-uksouth-local-network-gateway"
   resource_group_name = azurerm_resource_group.myAzureResourceGroup2.name
@@ -10,4 +10,4 @@ resource "azurerm_local_network_gateway" "myAzureLocalNetworkGateway" {
   gateway_address     = "145.43.244.136"
   address_space       = ["10.0.0.0/16"]
 }
-*/
+
