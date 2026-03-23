d6288149eaf09d2ee7e8499480674665d0891daa
cheung222000 <cheung222000@gmail.com>
2026-03-23 12:07:56 +0000

    Update vpngateway.tf

--- vpngateway.tf
+++ vpngateway.tf
@@ -41,7 +41,7 @@ resource "azurerm_subnet" "example" {
   address_prefixes     = ["10.0.1.0/24"]
   ##address_prefixes     = ["10.241.38.0/26"]
 }
-
+/*
 resource "azurerm_virtual_network_gateway" "myAzureVirtualNetworkGateway" {
   name                = "dataservices-nonprod-uksouth-virtual-network-gateway"
   location            = azurerm_resource_group.myAzureResourceGroup2.location
@@ -98,5 +98,5 @@ EOF
       thumbprint = "912198EEF23DCAC40939312FEE97DD560BAE49B1"
     }
   }
-}
+}*/
 
