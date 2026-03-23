8522bff739be032a509c1a410cd276eb83700260
cheung222000 <cheung222000@gmail.com>
2026-03-23 15:25:31 +0000

    Update vpngateway.tf

--- vpngateway.tf
+++ vpngateway.tf
@@ -24,21 +24,21 @@ resource "azurerm_public_ip" "myAzurePublicIP" {
 ###########################################################################
 
 resource "azurerm_virtual_network" "myAzureVirtualNetwork2" {
-  name                = "myAzureVirtualNetwork2"
-  #name                = "networks-production-uksouth-vnet"
+  #name                = "myAzureVirtualNetwork2"
+  name                = "networks-production-uksouth-vnet"
   location            = azurerm_resource_group.myAzureResourceGroup2.location
   resource_group_name = azurerm_resource_group.myAzureResourceGroup2.name
-  address_space       = ["10.0.0.0/16"]
-  #address_space       = ["10.241.38.0/24"]
+  #address_space       = ["10.0.0.0/16"]
+  address_space       = ["10.241.38.0/24"]
 }
 
-resource "azurerm_subnet" "example" {
-#resource "azurerm_subnet" "myAzureSubnet1" {
+#resource "azurerm_subnet" "example" {
+resource "azurerm_subnet" "myAzureSubnet1" {
   name                 = "GatewaySubnet"
   resource_group_name  = azurerm_resource_group.myAzureResourceGroup2.name
   virtual_network_name = azurerm_virtual_network.myAzureVirtualNetwork2.name
-  address_prefixes     = ["10.0.1.0/24"]
-  ##address_prefixes     = ["10.241.38.0/26"]
+  #address_prefixes     = ["10.0.1.0/24"]
+  address_prefixes     = ["10.241.38.0/26"]
 }
 /*
 resource "azurerm_virtual_network_gateway" "myAzureVirtualNetworkGateway" {
