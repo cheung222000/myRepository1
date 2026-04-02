f14b0eed3d6910031919f7f6064a38be1d9f4dee
cheung222000 <cheung222000@gmail.com>
2026-04-02 16:22:01 +0100

    Create hubs.tf

---
+++ hubs.tf
@@ -0,0 +1,17 @@
+module "metadata" {
+  #source   = "github.com/Azure-Terraform/terraform-azurerm-metadata.git?ref=v1.5.3"
+  for_each = local.hubs
+
+  #naming_rules = module.naming.yaml
+
+  market              = "us"
+  project             = "Global Network Hub"
+  #location            = each.value.region
+  environment         = "prod"
+  product_name        = "expressroute"
+  business_unit       = "iog"
+  product_group       = "networks"
+  #subscription_id     = data.azurerm_subscription.current.subscription_id
+  subscription_type   = "production"
+  resource_group_type = "shared"
+}
