585852d5662eecfb56c0a000a6247757678e788d
cheung222000 <cheung222000@gmail.com>
2026-04-02 16:41:15 +0100

    Update hubs.tf

--- hubs.tf
+++ hubs.tf
@@ -1,17 +1,18 @@
 module "metadata" {
-  source   = "github.com/Azure-Terraform/terraform-azurerm-metadata.git?ref=v1.5.3"
+  #source   = "github.com/Azure-Terraform/terraform-azurerm-metadata.git?ref=v1.5.3"
+  source   = "./data/hubs.yaml"
   for_each = local.hubs
 
-  naming_rules = module.naming.yaml
+  #naming_rules = module.naming.yaml
 
   market              = "us"
   project             = "Global Network Hub"
-  location            = each.value.region
+  #location            = each.value.region
   environment         = "prod"
   product_name        = "expressroute"
   business_unit       = "iog"
   product_group       = "networks"
-  subscription_id     = data.azurerm_subscription.current.subscription_id
+  #subscription_id     = data.azurerm_subscription.current.subscription_id
   subscription_type   = "production"
   resource_group_type = "shared"
 }
