097833f6650511408add135e6341e95cc88fdd0c
cheung222000 <cheung222000@gmail.com>
2026-04-03 11:39:51 +0100

    Update variables.tf

--- modules/metadata/variables.tf
+++ modules/metadata/variables.tf
@@ -1,3 +1,8 @@
+variable "naming_rules" {
+  description = "naming conventions yaml file" 
+  type        = string
+}
+
 variable "market" {
   description = "rba.market (https://github.com/Azure-Terraform/example-naming-template#custommarket)"
   type        = string
