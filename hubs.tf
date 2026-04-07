612c5ce017e682048f375f2eb17c6c8dae96d593
cheung222000 <cheung222000@gmail.com>
2026-04-07 11:49:45 +0100

    Update hubs.tf

--- hubs.tf
+++ hubs.tf
@@ -5,12 +5,10 @@ module "metadata" {
 
   market              = "us"
   project             = "Global Network Hub"
-  #location            = "us"
   environment         = "prod"
   product_name        = "expressroute"
   business_unit       = "iog"
   product_group       = "networks"
   subscription_id     = "subid
   subscription_type   = "production"
-  
 }
