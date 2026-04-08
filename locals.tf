a88aee97ae5710aea66c399c43b819765b23b672
cheung222000 <cheung222000@gmail.com>
2026-04-08 10:12:06 +0100

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -283,10 +283,11 @@ vpn_gateway_ranges_map = tomap({ for r in local.vpn_gateway_ranges : "${r.primar
                                             */
 
 }
-/*
+
 output "hub_data_debug" {
-  value = local.vpn_gateway_ranges_map
+  value = local.vpn_gateway_ranges_map.value.hub.name
 }
+/*
 output "hub_data_debug2" {
   value = jsonencode(local.vpn_gateway_ranges_map)
 }
