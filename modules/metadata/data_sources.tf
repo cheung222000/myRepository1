88dd4fb45ad08d4f7472504f5eaf8aae209dbebb
cheung222000 <cheung222000@gmail.com>
2026-04-08 14:23:25 +0100

    Update data_sources.tf

--- modules/metadata/data_sources.tf
+++ modules/metadata/data_sources.tf
@@ -1,3 +1,3 @@
 locals {
-  naming_rules = yamldecode(var.naming_rules) #把外部傳進來的 YAML（字串）轉成 Terraform 可操作的 object，存在 local.naming_rules。
+  naming_rules = yamldecode(var.naming_rules) #convert YAML to Terraform operable-object
 }
