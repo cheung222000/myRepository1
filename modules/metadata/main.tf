99b115f0c81000d55877d67e1b3830f97b9de518
cheung222000 <cheung222000@gmail.com>
2026-04-13 16:23:51 +0100

    Update main.tf

--- modules/metadata/main.tf
+++ modules/metadata/main.tf
@@ -12,13 +12,13 @@ locals {
       market            = var.market
       subscription_type = var.subscription_type
     }, #optional values
-    var.on_prem != "" ? { on_prem = var.on_prem } : {},
-    var.product_group != "" ? { product_group = var.product_group } : {},
-    var.product_name != "" ? { product_name = var.product_name } : {},
-    var.resource_group_type != "" ? { resource_group_type = var.resource_group_type } : {},
-    var.service_name != "" ? { service_name = var.service_name } : {},
-    var.subnet_type != "" ? { subnet_type = var.subnet_type } : {},
-    var.virtual_network_gateway_type != "" ? { virtual_network_gateway_type = var.virtual_network_gateway_type } : {},
+    #var.on_prem != "" ? { on_prem = var.on_prem } : {},
+    #var.product_group != "" ? { product_group = var.product_group } : {},
+    #var.product_name != "" ? { product_name = var.product_name } : {},
+    #var.resource_group_type != "" ? { resource_group_type = var.resource_group_type } : {},
+    #var.service_name != "" ? { service_name = var.service_name } : {},
+    #var.subnet_type != "" ? { subnet_type = var.subnet_type } : {},
+    #var.virtual_network_gateway_type != "" ? { virtual_network_gateway_type = var.virtual_network_gateway_type } : {},
   )
 /*
   names = merge(
