1f0cca2c8581d6f71021521e06441a38846fbac8
cheung222000 <cheung222000@gmail.com>
2026-04-14 09:41:28 +0100

    Update main.tf

--- modules/metadata/main.tf
+++ modules/metadata/main.tf
@@ -20,23 +20,5 @@ locals {
     #var.subnet_type != "" ? { subnet_type = var.subnet_type } : {},
     #var.virtual_network_gateway_type != "" ? { virtual_network_gateway_type = var.virtual_network_gateway_type } : {},
   )
-/*
-  names = merge(
-    {
-      business_unit     = var.business_unit
-      environment       = var.environment
-      location          = var.location
-      market            = var.market
-      subscription_type = var.subscription_type
-    },
-    var.on_prem != "" ? { on_prem = var.on_prem } : {},
-    var.product_group != "" ? { product_group = var.product_group } : {},
-    var.product_name != "" ? { product_name = var.product_name } : {},
-    var.resource_group_type != "" ? { resource_group_type = var.resource_group_type } : {},
-    var.service_name != "" ? { service_name = var.service_name } : {},
-    var.subnet_type != "" ? { subnet_type = var.subnet_type } : {},
-    var.virtual_network_gateway_type != "" ? { virtual_network_gateway_type = var.virtual_network_gateway_type } : {},
-  )
-*/
   
 }
