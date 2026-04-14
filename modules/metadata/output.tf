bfbe82d3cbe18bc5e67cab4bb1d3ffb9ed063a81
cheung222000 <cheung222000@gmail.com>
2026-04-14 16:26:37 +0100

    Update output.tf

--- modules/metadata/output.tf
+++ modules/metadata/output.tf
@@ -7,3 +7,8 @@ output "names" {
   value       = local.names
   description = "Map of validated names for resources"
 }
+
+output "tags" {
+  value       = local.tags
+  description = "Map of tags with any global defaults appended"
+}
