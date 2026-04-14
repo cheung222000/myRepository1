07ce12a92df63afcb43707989b0c8210908cf863
cheung222000 <cheung222000@gmail.com>
2026-04-14 16:01:00 +0100

    Update main.tf

--- modules/metadata/main.tf
+++ modules/metadata/main.tf
@@ -32,14 +32,14 @@ locals {
       subscription_type = local.naming_rules.subscriptionType.allowed_values[var.subscription_type]
       project           = var.project
     }, #optional values
-    #var.on_prem != "" ? { on_prem = local.naming_rules.onPrem.allowed_values[var.on_prem] } : {},
-    #var.product_group != "" ? { product_group = lookup(local.naming_rules.productGroup.allowed_values, var.product_group, var.product_group) } : {},
-    #var.product_name != "" ? { product_name = lookup(local.naming_rules.productName.allowed_values, var.product_name, var.product_name) } : {},
-    #var.resource_group_type != "" ? { resource_group_type = local.naming_rules.resourceGroupType.allowed_values[var.resource_group_type] } : {},
-    #var.service_name != "" ? { service_name = local.naming_rules.serviceName.allowed_values[var.service_name] } : {},
+    var.on_prem != "" ? { on_prem = local.naming_rules.onPrem.allowed_values[var.on_prem] } : {},
+    var.product_group != "" ? { product_group = lookup(local.naming_rules.productGroup.allowed_values, var.product_group, var.product_group) } : {},
+    var.product_name != "" ? { product_name = lookup(local.naming_rules.productName.allowed_values, var.product_name, var.product_name) } : {},
+    var.resource_group_type != "" ? { resource_group_type = local.naming_rules.resourceGroupType.allowed_values[var.resource_group_type] } : {},
+    var.service_name != "" ? { service_name = local.naming_rules.serviceName.allowed_values[var.service_name] } : {},
     #var.sre_team != "" ? { sre_team = var.sre_team } : {},
-    #var.subnet_type != "" ? { subnet_type = local.naming_rules.subnetType.allowed_values[var.subnet_type] } : {},
-    #var.virtual_network_gateway_type != "" ? { virtual_network_gateway_type = local.naming_rules.virtualNetGwType.allowed_values[var.virtual_network_gateway_type] } : {},
+    var.subnet_type != "" ? { subnet_type = local.naming_rules.subnetType.allowed_values[var.subnet_type] } : {},
+    var.virtual_network_gateway_type != "" ? { virtual_network_gateway_type = local.naming_rules.virtualNetGwType.allowed_values[var.virtual_network_gateway_type] } : {},
     #var.additional_tags,
     #local.default_tags,
   )
