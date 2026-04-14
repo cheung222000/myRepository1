d7ba69a8d9eac263b05732d56cc3eba6bfa9763e
cheung222000 <cheung222000@gmail.com>
2026-04-14 10:10:24 +0100

    Update output.tf

--- modules/metadata/output.tf
+++ modules/metadata/output.tf
@@ -2,3 +2,8 @@ output "location" {
   value       = local.location
   description = "Azure region"
 }
+
+output "names" {
+  value       = local.names
+  description = "Map of validated names for resources"
+}
