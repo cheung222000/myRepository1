0db2186b210628a64972fd5ad46a88b2adbe8d9b
cheung222000 <cheung222000@gmail.com>
2026-04-15 15:44:58 +0100

    Update locals.tf

--- modules/resource_group/locals.tf
+++ modules/resource_group/locals.tf
@@ -1,4 +1,4 @@
 locals {
-  name_suffix_safe  = lower(var.name_suffix == "" ? "" : "-${var.name_suffix}")
+  name_suffix_safe  = lower(var.name_suffix == "" ? "" : "-${var.name_suffix}") //if empty then empty, not then lower case and add a -
 
 }
