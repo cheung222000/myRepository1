493d675d64ab6926d251536606e5894c5a88e064
cheung222000 <cheung222000@gmail.com>
2026-04-16 16:53:17 +0100

    Update locals.tf

--- modules/resource_group/locals.tf
+++ modules/resource_group/locals.tf
@@ -1,4 +1,4 @@
 locals {
   name_suffix_safe  = lower(var.name_suffix == "" ? "" : "-${var.name_suffix}") //if empty then empty, not then lower case and add a -
-  resource_group_name = "${var.names.resource_group_type}-${var.names.product_name}-${var.names.environment}-${var.names.location}${local.name_suffix_safe}"
+  resource_group_name = "${var.names.resource_group_type}-${var.names.product_name}-${var.names.environment}-${var.names.location}${local.name_suffix_safe}"  #shared-expressroute-prod-uksouth
 }
