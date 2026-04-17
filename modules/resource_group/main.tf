03e8caff5276eb18df4014e363a62ca9f6becf4d
cheung222000 <cheung222000@gmail.com>
2026-04-17 11:25:23 +0100

    Update main.tf

--- modules/resource_group/main.tf
+++ modules/resource_group/main.tf
@@ -1,1 +1,8 @@
 #https://github.com/Azure-Terraform/terraform-azurerm-resource-group?ref=v2.1.1
+
+resource "random_integer" "suffix" {
+  count = var.unique_name == "true" ? 1 : 0
+
+  min = 10000
+  max = 99999
+}
