1c75c27ea8320239baf94de5c2ec39b4f3012aeb
cheung222000 <cheung222000@gmail.com>
2026-04-17 15:14:19 +0100

    Update vpngateway.tf

--- vpngateway.tf
+++ vpngateway.tf
@@ -32,8 +32,8 @@ resource "azurerm_public_ip" "vpngateway" {
   for_each = { for key, value in local.vpn_gateway_ranges_map : key => value }
   
   name                = "${each.key}-vpngateway-ip"
-  resource_group_name = azurerm_resource_group.myAzureResourceGroup2.name
-  #resource_group_name = module.resource_group[each.value.hub].name
+  #resource_group_name = azurerm_resource_group.myAzureResourceGroup2.name
+  resource_group_name = module.resource_group[each.value.hub].name
   location            = module.metadata[each.value.hub].location                  #each.value.hub = "dataservices-nonprod-uksouth"
   allocation_method   = "Static"
   zones         = [1, 2, 3]
