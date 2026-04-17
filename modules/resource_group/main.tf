a7380de4773e502093590b5795acdd708a231854
cheung222000 <cheung222000@gmail.com>
2026-04-17 11:41:29 +0100

    Update main.tf

--- modules/resource_group/main.tf
+++ modules/resource_group/main.tf
@@ -1,7 +1,7 @@
 #https://github.com/Azure-Terraform/terraform-azurerm-resource-group?ref=v2.1.1
 
 resource "random_integer" "suffix" {
-  count = var.unique_name == "true" ? 1 : 0
+  count = var.unique_name == "true" ? 1 : 0  #if count is existed, the random_integer.suffix is a list. Call this list need to add index[0] for the first list item
 
   min = 10000
   max = 99999
