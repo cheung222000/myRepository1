da2a160eea9d34784d651e2585090098735541a3
cheung222000 <cheung222000@gmail.com>
2026-04-17 15:51:56 +0100

    Update main.tf

--- modules/metadata/main.tf
+++ modules/metadata/main.tf
@@ -31,13 +31,13 @@ locals {
   # Tagging metadata
   tags = merge( #creating object
     { #mandatory values
-      business_unit     = local.naming_rules.businessUnit.allowed_values[var.business_unit]
-      environment       = local.naming_rules.environment.allowed_values[var.environment]
-      location          = local.naming_rules.azureRegion.allowed_values[var.location]
-      market            = local.naming_rules.market.allowed_values[var.market]
-      subscription_id   = var.subscription_id
-      subscription_type = local.naming_rules.subscriptionType.allowed_values[var.subscription_type]
-      project           = var.project
+      business_unit     = local.naming_rules.businessUnit.allowed_values[var.business_unit]                      #iog
+      environment       = local.naming_rules.environment.allowed_values[var.environment]                         #prod
+      location          = local.naming_rules.azureRegion.allowed_values[var.location]                            #
+      market            = local.naming_rules.market.allowed_values[var.market]                                   #us
+      subscription_id   = var.subscription_id                                                                    #xxxxxxxxxxxxxxxx
+      subscription_type = local.naming_rules.subscriptionType.allowed_values[var.subscription_type]              #production
+      project           = var.project                                                                            #Global Network Hub
     }, #optional values
     var.on_prem != "" ? { on_prem = local.naming_rules.onPrem.allowed_values[var.on_prem] } : {},
     var.product_group != "" ? { product_group = lookup(local.naming_rules.productGroup.allowed_values, var.product_group, var.product_group) } : {},
