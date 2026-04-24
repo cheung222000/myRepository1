d3e0c87fa59354403525960fe4ddadf861bd6a21
cheung222000 <cheung222000@gmail.com>
2026-04-24 15:26:51 +0100

    Create variables.tf

---
+++ modules/virtual_network/variables.tf
@@ -0,0 +1,5 @@
+variable "virtual_network_name" {
+  description = "Optional override for the virtual network name. If not set, name is generated from other variables."
+  type        = string
+  default     = null
+}
