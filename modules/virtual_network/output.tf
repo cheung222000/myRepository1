194159232f2d80eaa2ffbad8c160ed3e9bfb3cd3
cheung222000 <cheung222000@gmail.com>
2026-05-04 15:41:39 +0100

    Update output.tf

--- modules/virtual_network/output.tf
+++ modules/virtual_network/output.tf
@@ -1,1 +1,8 @@
 ##
+output "subnet" {
+  description = "Map of subnet data objects."
+  value = zipmap(
+    [for subnet in module.subnet : subnet.name],
+    [for subnet in module.subnet : subnet.subnet]
+  )
+}
