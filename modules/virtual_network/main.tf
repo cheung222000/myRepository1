5324094aaf9c2406562d866e9294345fa344aba4
cheung222000 <cheung222000@gmail.com>
2026-05-15 16:15:24 +0100

    Update main.tf

--- modules/virtual_network/main.tf
+++ modules/virtual_network/main.tf
@@ -4,3 +4,38 @@ module "subnet" {
   source   = "./subnet"
   for_each = local.subnets
 }
+
+/*
+					  subnets = {
+						"GatewaySubnet" = {
+							cidrs                                         = [local.hub_cidrs[each.key].subnets[0]]
+							private_endpoint_network_policies             = "true"
+							private_link_service_network_policies_enabled = true
+							service_endpoints                             = []
+							delegations                                   = {}
+							create_network_security_group                 = false
+							security_group_prefix                         = null
+							configure_nsg_rules                           = true
+							allow_internet_outbound                       = false
+							allow_lb_inbound                              = false
+							allow_vnet_inbound                            = false
+							allow_vnet_outbound                           = false
+							route_table_association                       = null
+						},
+						"AzureFirewallSubnet" = {
+							cidrs                                         = [local.hub_cidrs[each.key].subnets[1]]
+							private_endpoint_network_policies             = "true"
+							private_link_service_network_policies_enabled = true
+							service_endpoints                             = []
+							delegations                                   = {}
+							create_network_security_group                 = false
+							security_group_prefix                         = null
+							configure_nsg_rules                           = true
+							allow_internet_outbound                       = false
+							allow_lb_inbound                              = false
+							allow_vnet_inbound                            = false
+							allow_vnet_outbound                           = false
+							route_table_association                       = null
+						},
+					  }
+*/
