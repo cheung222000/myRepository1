7312b220c810cf8547d526c744844c6b3c7e513d
cheung222000 <cheung222000@gmail.com>
2026-05-18 11:48:03 +0100

    Update variables.tf

--- modules/virtual_network/subnet/variables.tf
+++ modules/virtual_network/subnet/variables.tf
@@ -8,12 +8,12 @@ variable "location" {
   description = "Azure Region"
   type        = string
 }
-
+*/
 variable "names" {
   description = "names to be applied to resources"
   type        = map(string)
 }
-
+/*
 variable "tags" {
   description = "tags to be applied to resources"
   type        = map(string)
