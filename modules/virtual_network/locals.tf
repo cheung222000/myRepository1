ab67c91948e15ebb1c60f07293953a0afcaf1892
cheung222000 <cheung222000@gmail.com>
2026-05-19 15:45:47 +0100

    Update locals.tf

--- modules/virtual_network/locals.tf
+++ modules/virtual_network/locals.tf
@@ -1,4 +1,5 @@
 locals {
+  virtual_network_name = (var.virtual_network_name != null && var.virtual_network_name != "" ? var.virtual_network_name : (var.use_product_name ? "${var.names.product_name}-${var.names.subscription_type}-${var.names.location}-vnet" : "${var.names.product_group}-${var.names.subscription_type}-${var.names.location}-vnet"))
   subnets = zipmap(keys(var.subnets), [for subnet in values(var.subnets) : merge(var.subnet_defaults, subnet)])
 
 /*
