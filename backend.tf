e60c82efa4cbe61d06e86a0493fcc4b077fb639e
cheung222000 <cheung222000@gmail.com>
2026-05-21 11:51:53 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -7,7 +7,7 @@ terraform {
   }
   backend "azurerm" {
     resource_group_name  = "terraform-rg"
-    storage_account_name = "tfstate889243219119"
+    storage_account_name = "tfstate889243219120"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
