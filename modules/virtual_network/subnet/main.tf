45f783dfbe2eb1be625d73068b8d841592389eb6
cheung222000 <cheung222000@gmail.com>
2026-05-22 15:36:28 +0100

    Update main.tf

--- modules/virtual_network/subnet/main.tf
+++ modules/virtual_network/subnet/main.tf
@@ -1,1 +1,27 @@
-##
+/*
+resource "azurerm_subnet" "subnet" {
+  name                 = var.subnet_type
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = var.virtual_network_name
+  address_prefixes     = var.cidrs
+
+  private_endpoint_network_policies             = var.private_endpoint_network_policies
+  private_link_service_network_policies_enabled = var.private_link_service_network_policies_enabled
+
+
+  service_endpoints = var.service_endpoints
+
+  default_outbound_access_enabled = var.default_outbound_access_enabled
+
+  dynamic "delegation" {
+    for_each = var.delegations
+    content {
+      name = delegation.key
+      service_delegation {
+        name    = delegation.value.name
+        actions = delegation.value.actions
+      }
+    }
+  }
+}
+*/
