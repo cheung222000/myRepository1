0e8ccf3aa81756b514668ad38222e1aac335f10d
cheung222000 <cheung222000@gmail.com>
2026-05-26 15:53:43 +0100

    Update output.tf

--- modules/virtual_network/subnet/output.tf
+++ modules/virtual_network/subnet/output.tf
@@ -8,7 +8,7 @@ output "name" {
   description = "subnet name"
   value       = azurerm_subnet.subnet.name
 }
-##
+/*
 output "network_security_group_id" {
   description = "network security group id"
   value       = (var.create_network_security_group ? azurerm_network_security_group.nsg.0.id : null)
@@ -18,7 +18,7 @@ output "network_security_group_name" {
   description = "network security group name"
   value       = (var.create_network_security_group ? azurerm_network_security_group.nsg.0.name : null)
 }
-
+*/
 output "subnet" {
   description = "subnet data object"
   value       = azurerm_subnet.subnet
