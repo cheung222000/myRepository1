926e280ccb02c0a752a194ccb631b0f6234a6a8f
cheung222000 <cheung222000@gmail.com>
2026-05-28 15:28:19 +0100

    Update main.tf

--- modules/virtual_network/subnet/main.tf
+++ modules/virtual_network/subnet/main.tf
@@ -32,7 +32,7 @@ resource "azurerm_subnet_network_security_group_association" "subnet_nsg" {
   network_security_group_id = azurerm_network_security_group.nsg.0.id
 }
 */
-#
+
 resource "azurerm_network_security_group" "nsg" {
   count = (var.create_network_security_group ? 1 : 0)
 
@@ -41,7 +41,7 @@ resource "azurerm_network_security_group" "nsg" {
   resource_group_name = var.resource_group_name
   tags                = merge(var.tags, { subnet_type = lookup(local.allowed_subnet_info, var.subnet_type, var.subnet_type) })
 }
-/*
+
 resource "azurerm_network_security_rule" "deny_all_inbound" {
   count = ((var.create_network_security_group && var.configure_nsg_rules) ? 1 : 0)
 
@@ -57,7 +57,7 @@ resource "azurerm_network_security_rule" "deny_all_inbound" {
   resource_group_name         = var.resource_group_name
   network_security_group_name = azurerm_network_security_group.nsg.0.name
 }
-
+/*
 resource "azurerm_network_security_rule" "deny_all_outbound" {
   count = ((var.create_network_security_group && var.configure_nsg_rules) ? 1 : 0)
 
